a7abaf8f6d3655dd70cbdff7987f214b114171a0
Seb121291 <seb_ziegler@gmx.net>
2026-04-01 11:00:12 +0200

    Grundstrukturen in die Dateien eingefügt

---
+++ vite-project/src/components/InputSection.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+export default function InputSection() {
+  return (
+    <div>InputSection</div>
+  )
+}
---
+++ vite-project/src/components/TodoList.jsx
@@ -0,0 +1,12 @@
+import Todo from "./Todo"
+export default function TodoList() {
+  return (
+    <>
+    <p>Start List:</p>
+    {/* Hier wird gemappt
+    
+    */}
+    <Todo />
+    </>
+  )
+}
